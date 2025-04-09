62ad67e57b96afd9b09e370055fff21cd2c030c4
신관규 <soundbar91@gmail.com>
2025-04-09 10:34:33 +0900

    feat: 기본 접속 주소 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
